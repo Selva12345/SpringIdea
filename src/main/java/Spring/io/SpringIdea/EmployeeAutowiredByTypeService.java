17cf709cd1cb5bbc50653e8a99e22781c424ca54
selvaganapathi.g <selvaganapathi.g@IMPC1952.htcindia.local>
2019-06-07 15:01:51 +0530

    code

---
+++ src/main/java/Spring/io/SpringIdea/EmployeeAutowiredByTypeService.java
@@ -0,0 +1,20 @@
+package Spring.io.SpringIdea;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EmployeeAutowiredByTypeService {
+
+	//Autowired annotation on variable/setters is equivalent to autowire="byType"
+	@Autowired
+	private Employee employee;
+	
+	
+	@Autowired
+	public void setEmployee(Employee emp){
+		this.employee=emp;
+	}
+	
+	public Employee getEmployee(){
+		return this.employee;
+	}
+}
